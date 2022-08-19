d6aac61d678ea93e90d831727d32ed9e5e9769c7
Justin Awrey <awreyjustin@gmail.com>
2022-08-19 10:35:33 -0700

    add main example

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { serve } from "./deps.ts";
+import fsRouter from "./mod.ts";
+
+if (!import.meta.main) {
+  Deno.exit();
+}
+
+if (Deno.args.length !== 1) {
+  console.log("Usage: deno run --allow-read --allow-net main.ts <dir>");
+  Deno.exit();
+}
+
+const dir = Deno.args[0];
+serve(await fsRouter(dir));
