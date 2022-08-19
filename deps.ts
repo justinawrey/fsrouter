87d295d22007b32ae7ad1ac2bfc3bf4b6b43b686
Justin Awrey <awreyjustin@gmail.com>
2022-08-19 10:35:40 -0700

    fix deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,9 @@
-export { extname, join } from "https://deno.land/std@0.152.0/path/posix.ts";
+export {
+  extname,
+  join,
+  parse,
+  relative,
+} from "https://deno.land/std@0.152.0/path/posix.ts";
 export { walk } from "https://deno.land/std@0.152.0/fs/walk.ts";
 export {
   type Handler,
