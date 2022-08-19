6ab6f54340c5044d05bf5eaffb8f40e6f0f1811c
Justin Awrey <awreyjustin@gmail.com>
2022-08-19 10:41:02 -0700

    working

--- mod.ts
+++ mod.ts
@@ -1,29 +1,31 @@
-import { Handler, join, serve, walk } from "./deps.ts";
+import { Handler, join, parse, relative, walk } from "./deps.ts";
 
-const pathMap = new Map<string, Handler>();
+export default async function fsRouter(path: string): Promise<Handler> {
+  const pathMap = new Map<string, Handler>();
 
-for await (const entry of walk("example", { includeDirs: false })) {
-  const { path } = entry;
+  for await (const entry of walk(path, { includeDirs: false, exts: [".ts"] })) {
+    const parsedPath = parse(entry.path);
 
-  console.log(path);
+    const relativeDir = relative(path, parsedPath.dir);
+    let out = join(parsedPath.root, relativeDir, parsedPath.name);
+    if (out.endsWith("index")) {
+      out = out.slice(0, -5);
+    }
 
-  const route = path.replace("example", "").replace(".ts", "");
+    if (out !== "/" && out.endsWith("/")) {
+      out = out.slice(0, -1);
+    }
 
-  const handler = await import(join(Deno.cwd(), path));
-  pathMap.set(route, handler.default);
+    const handler = await import(entry.path);
+    pathMap.set(out, handler.default);
+  }
+
+  return (req, connInfo) => {
+    const { pathname } = new URL(req.url);
+
+    if (!pathMap.has(pathname)) return new Response("404");
+
+    const handler = pathMap.get(pathname) as Handler;
+    return handler(req, connInfo);
+  };
 }
-
-const handleRequest: Handler = (req, connInfo) => {
-  const { pathname } = new URL(req.url);
-
-  console.log(pathname);
-
-  if (!pathMap.has(pathname)) return new Response("404");
-
-  const handler = pathMap.get(pathname) as Handler;
-  return handler(req, connInfo);
-};
-
-console.log(pathMap);
-
-serve(handleRequest);
