32542b91f3fc8049ff224474f7850c1b44ce9280
Justin Awrey <awreyjustin@gmail.com>
2022-08-21 16:37:23 -0700

    fix: resolve path issues when running example directly from remote url

--- _scripts/example.ts
+++ _scripts/example.ts
@@ -1,10 +1,8 @@
 import { serve } from "../private/deps/std/http.ts";
-import { fromFileUrl } from "../private/deps/std/path.ts";
 import { fsRouter } from "../mod.ts";
 
 if (!import.meta.main) {
   Deno.exit();
 }
 
-const rootDir = fromFileUrl(import.meta.resolve("./pages"));
-serve(await fsRouter(rootDir));
+serve(await fsRouter("pages"));
--- private/deps/std/path.ts
+++ private/deps/std/path.ts
@@ -1,5 +1,4 @@
 export {
-  fromFileUrl,
   relative,
   resolve,
   toFileUrl,
