5c0b8bc3ada1200d516b0ad08ac7a58eff87adab
Justin Awrey <awreyjustin@gmail.com>
2022-08-31 16:23:21 -0700

    ci: move to .releaserc.js

---
+++ .releaserc.js
@@ -0,0 +1,24 @@
+export default {
+  branches: ["main"],
+  plugins: [
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    [
+      "@semantic-release/release-notes-generator",
+      { preset: "conventionalcommits" },
+    ],
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.gitTag} [skip ci]\\n\\n${nextRelease.notes}",
+      },
+    ],
+    ["@semantic-release/exec", {
+      publishCmd: `echo 'export const version = "\${nextRelease.version}'" > version.ts`,
+    }],
+  ],
+  tagFormat: "${version}",
+};
