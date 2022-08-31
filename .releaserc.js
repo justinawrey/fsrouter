a5775a54120fbdfb35fbe96cbf4910e40082e2ea
Justin Awrey <awreyjustin@gmail.com>
2022-08-31 16:34:44 -0700

    ci: write version.ts in semantic-release exec

--- .releaserc.js
+++ .releaserc.js
@@ -7,19 +7,19 @@ export default {
       { preset: "conventionalcommits" },
     ],
     "@semantic-release/changelog",
+    ["@semantic-release/exec", {
+      prepareCmd:
+        `echo 'export const version = "\${nextRelease.version}'" > version.ts`,
+    }],
     "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md"],
+        assets: ["CHANGELOG.md", "version.ts"],
         message:
           "chore(release): ${nextRelease.gitTag} [skip ci]\\n\\n${nextRelease.notes}",
       },
     ],
-    ["@semantic-release/exec", {
-      publishCmd:
-        `echo 'export const version = "\${nextRelease.version}'" > version.ts`,
-    }],
   ],
   tagFormat: "${version}",
 };
