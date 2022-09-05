3f484f6d5dcbe3561addeab2ebf244fbc553647a
Justin Awrey <awreyjustin@gmail.com>
2022-09-04 18:06:50 -0700

    chore: update std ver to 0.154.0

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * as colors from "https://deno.land/std@0.153.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.153.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.153.0/path/mod.ts";
-export * as http from "https://deno.land/std@0.153.0/http/mod.ts";
-export * as log from "https://deno.land/std@0.153.0/log/mod.ts";
-export * as flags from "https://deno.land/std@0.153.0/flags/mod.ts";
-export * as asserts from "https://deno.land/std@0.153.0/testing/asserts.ts";
+export * as colors from "https://deno.land/std@0.154.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.154.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.154.0/path/mod.ts";
+export * as http from "https://deno.land/std@0.154.0/http/mod.ts";
+export * as log from "https://deno.land/std@0.154.0/log/mod.ts";
+export * as flags from "https://deno.land/std@0.154.0/flags/mod.ts";
+export * as asserts from "https://deno.land/std@0.154.0/testing/asserts.ts";
