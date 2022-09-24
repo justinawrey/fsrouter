520d625070c81e6a0054b8aefbbabcf5a605ae64
Justin Awrey <awreyjustin@gmail.com>
2022-09-23 18:09:32 -0700

    chore: go back to 0.156.0

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * as colors from "https://deno.land/std@0.157.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.157.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.157.0/path/mod.ts";
-export * as http from "https://deno.land/std@0.157.0/http/mod.ts";
-export * as log from "https://deno.land/std@0.157.0/log/mod.ts";
-export * as flags from "https://deno.land/std@0.157.0/flags/mod.ts";
-export * as asserts from "https://deno.land/std@0.157.0/testing/asserts.ts";
+export * as colors from "https://deno.land/std@0.156.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.156.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.156.0/path/mod.ts";
+export * as http from "https://deno.land/std@0.156.0/http/mod.ts";
+export * as log from "https://deno.land/std@0.156.0/log/mod.ts";
+export * as flags from "https://deno.land/std@0.156.0/flags/mod.ts";
+export * as asserts from "https://deno.land/std@0.156.0/testing/asserts.ts";
