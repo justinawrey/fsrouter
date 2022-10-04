d37cb4723dcf16e419f21f69e705a0d19709ef8a
Justin Awrey <awreyjustin@gmail.com>
2022-10-03 19:17:48 -0700

    chore: update to std@0.158.0

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
 // Std
-export * as colors from "https://deno.land/std@0.156.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.156.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.156.0/path/mod.ts";
-export * as http from "https://deno.land/std@0.156.0/http/mod.ts";
-export * as log from "https://deno.land/std@0.156.0/log/mod.ts";
-export * as flags from "https://deno.land/std@0.156.0/flags/mod.ts";
-export * as asserts from "https://deno.land/std@0.156.0/testing/asserts.ts";
+export * as colors from "https://deno.land/std@0.158.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.158.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.158.0/path/mod.ts";
+export * as http from "https://deno.land/std@0.158.0/http/mod.ts";
+export * as log from "https://deno.land/std@0.158.0/log/mod.ts";
+export * as flags from "https://deno.land/std@0.158.0/flags/mod.ts";
+export * as asserts from "https://deno.land/std@0.158.0/testing/asserts.ts";
 
 // Third-party
 export { default as jsonTree } from "https://deno.land/x/jsontree@0.5.0/mod.ts";
